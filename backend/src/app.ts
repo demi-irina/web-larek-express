85ad32c118e435bdb1e2799de922c01df9b7dd0c
Irina <demi.irina.96@gmail.com>
2026-09-09 12:33:07 +0300

    feat: add request and error logging

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import path from 'path';
 import { DB_ADDRESS, PORT } from './config';
 import NotFoundError from './errors/not-found-error';
 import errorHandler from './middlewares/error-handler';
+import { errorLogger, requestLogger } from './middlewares/logger';
 import orderRouter from './routes/order';
 import productRouter from './routes/product';
 
@@ -13,10 +14,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/product', productRouter);
 app.use('/order', orderRouter);
 app.use((_req, _res, next) => next(new NotFoundError('Маршрут не найден')));
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
